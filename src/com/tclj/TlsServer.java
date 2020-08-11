a4307cb369aac76603a3c0c31a966d32ecdfcf03
elkromer <hkrome@live.unc.edu>
2020-08-11 18:01:14 -0400

    a lovely day at hinds feet

--- src/com/tclj/TlsServer.java
+++ src/com/tclj/TlsServer.java
@@ -1,17 +1,20 @@
 package com.tclj;
 import ipworks.*;
 
-import java.util.Dictionary;
-import java.util.TooManyListenersException;
+import java.util.Map;
+import java.io.IOException;
+import java.util.HashMap;
 
 public class TlsServer implements IpdaemonEventListener {
     private Ipdaemon daemon;
+    private Map<String, byte[]> inputBuffers;
 
     public TlsServer() {
         daemon = new Ipdaemon();
+        inputBuffers = new HashMap<String, byte[]>();
         try {
             daemon.addIpdaemonEventListener(this);
-        } catch (TooManyListenersException e) {
+        } catch (java.util.TooManyListenersException e) {
             throw new RuntimeException("Too many listeners. " + e.getMessage());
         } catch (Exception e) {
             throw new RuntimeException("Initialization failed. " + e.getMessage());
@@ -20,9 +23,9 @@ public class TlsServer implements IpdaemonEventListener {
 
     public void StartListening() {
         try {
-            daemon.setSSLCert(new Certificate(Certificate.cstPFXFile,"C:\\temp\\keys\\100.pfx", "test", "*"));
-            daemon.setSSLEnabled(true);
-            daemon.setLocalPort(2222);
+            //daemon.setSSLCert(new Certificate(Certificate.cstPFXFile,"C:\\temp\\keys\\100.pfx", "test", "*"));
+            //daemon.setSSLEnabled(true);
+            daemon.setLocalPort(22223);
             daemon.setListening(true);
         } catch (IPWorksException e) {
             throw new RuntimeException(e.getMessage());
@@ -34,6 +37,13 @@ public class TlsServer implements IpdaemonEventListener {
     public void DoAllEvents() {
         while (true) {
             try {
+                // System.out.println("Connected client list ===========");
+                // for (String keys : inputBuffers.keySet())
+                // {
+                //     System.out.println(keys);
+                // }
+                // System.out.println("========== ===========");
+                // java.lang.Thread.sleep(1000);
                 daemon.doEvents();
             } catch (IPWorksException e) {
                 throw new RuntimeException(e.getMessage());
@@ -55,7 +65,26 @@ public class TlsServer implements IpdaemonEventListener {
 
     @Override
     public void dataIn(IpdaemonDataInEvent ipdaemonDataInEvent) {
-
+        byte[] data = ipdaemonDataInEvent.text;
+        byte[] odata = inputBuffers.get(ipdaemonDataInEvent.connectionId);
+        
+        if (odata == null) {
+            inputBuffers.put(ipdaemonDataInEvent.connectionId, data);
+        } else {
+            // data already in the buffer
+            int newlength = data.length + odata.length;
+            byte[] newdata = new byte[newlength];
+            // append it
+            System.arraycopy(odata, 0, newdata, 0, odata.length);
+            System.arraycopy(data, 0, newdata, odata.length, data.length);
+            // update the buffer in the map
+            // try {
+            //     java.nio.file.Files.write(java.nio.file.Paths.get("C:\\temp\\tlsserver\\users\\" +ipdaemonDataInEvent.connectionId+ "\\sentdata.txt"), newdata);
+            // } catch (IOException e) {
+            //     System.out.println("IOException: " + e.getMessage());
+            // }
+            inputBuffers.put(ipdaemonDataInEvent.connectionId, newdata);
+        }
     }
 
     @Override
